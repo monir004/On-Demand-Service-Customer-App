5c894052427cf2cfa38a24ba7d473e2f227e47f4
sakib <sakib6900@gmail.com>
2018-06-17 01:04:10 +0600

    Test commit

--- app/src/main/java/com/dhakasetup/sakib/dhakasetupprototype/EditprofileActivity.java
+++ app/src/main/java/com/dhakasetup/sakib/dhakasetupprototype/EditprofileActivity.java
@@ -2,6 +2,7 @@ package com.dhakasetup.sakib.dhakasetupprototype;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class EditprofileActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class EditprofileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editprofile);
+        Toolbar toolbar;
     }
 }
